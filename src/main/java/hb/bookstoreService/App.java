7ceb44665e195643fd6a351625ffc4a34971c25c
svcgv <hb1230123@hotmail.com>
2018-10-02 09:34:52 +0800

    创建maven项目

---
+++ src/main/java/hb/bookstoreService/App.java
@@ -0,0 +1,13 @@
+package hb.bookstoreService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
